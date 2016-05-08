c9e67849569ee9eab4b4ab9d5718aba793d15250
Mark Lee <guardhaus@lazymalevolence.com>
2016-05-07 17:50:01 -0700

    Coveralls: ignore test files from code coverage

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -7,4 +7,4 @@ if test "$TRAVIS_OS_NAME" = "linux"; then travis-cargo --only stable doc-upload;
 # measure code coverage and upload to coveralls.io (the verify
 # argument mitigates kcov crashes due to malformed debuginfo, at the
 # cost of some speed <https://github.com/huonw/travis-cargo/issues/12>)
-travis-cargo coveralls --no-sudo --verify
+travis-cargo coveralls --no-sudo --verify --exclude-pattern=/test.rs

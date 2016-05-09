35058474d58c13737ca114a3f50a59a611815841
Mark Lee <guardhaus@lazymalevolence.com>
2016-05-09 09:00:13 -0700

    Travis: only upload docs & coverage with x64 stable builds

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -e
-# upload the documentation from the build with stable (automatically only actually
-# runs on the master branch, not individual PRs)
-if test "$TRAVIS_OS_NAME" = "linux"; then travis-cargo --only stable doc-upload; fi
-# measure code coverage and upload to coveralls.io (the verify
-# argument mitigates kcov crashes due to malformed debuginfo, at the
-# cost of some speed <https://github.com/huonw/travis-cargo/issues/12>)
-travis-cargo coveralls --no-sudo --verify --exclude-pattern=/test.rs,*.c
+if test "$TARGET" = "x86_64-unknown-linux-gnu"; then
+    # upload the documentation from the build with stable (automatically only actually
+    # runs on the master branch, not individual PRs)
+    travis-cargo --only stable doc-upload
+    # measure code coverage and upload to coveralls.io (the verify
+    # argument mitigates kcov crashes due to malformed debuginfo, at the
+    # cost of some speed <https://github.com/huonw/travis-cargo/issues/12>)
+    travis-cargo --only stable coveralls --no-sudo --verify --exclude-pattern=/test.rs,*.c
+fi

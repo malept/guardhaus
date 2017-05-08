4041bd723f9fcef2e8ef03f2d16b505082a04edc
Mark Lee <guardhaus@lazymalevolence.com>
2017-05-07 19:52:32 -0700

    Clean up clippy setup

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -6,7 +6,5 @@ set -e
 pip install 'travis-cargo<0.2' --user
 
 if test "$TRAVIS_RUST_VERSION" = "nightly"; then
-    if ! cargo --list | grep -q clippy; then
-        cargo install --git=https://github.com/arcnmx/cargo-clippy
-    fi
+    cargo install --force clippy
 fi

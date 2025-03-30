4dcb1cd37b84ee4c51b2128cddbb9b1a2ea98d29
malept <malept@users.noreply.github.com>
2025-03-30 06:34:35 +0000

    Publish

--- main/crypto_hash/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["Algorithm"],"fn":["digest","hex_digest"],"struct":["Hasher"]};
--- main/trait.impl/digest/core_api/trait.FixedOutputCore.js
+++ main/trait.impl/digest/core_api/trait.FixedOutputCore.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["digest",[]],["sha1",[["impl <a class=\"trait\" href=\"digest/core_api/trait.FixedOutputCore.html\" title=\"trait digest::core_api::FixedOutputCore\">FixedOutputCore</a> for <a class=\"struct\" href=\"sha1/struct.Sha1Core.html\" title=\"struct sha1::Sha1Core\">Sha1Core</a>"]]]]);
+    var implementors = Object.fromEntries([["digest",[]],["md5",[["impl <a class=\"trait\" href=\"digest/core_api/trait.FixedOutputCore.html\" title=\"trait digest::core_api::FixedOutputCore\">FixedOutputCore</a> for <a class=\"struct\" href=\"md5/struct.Md5Core.html\" title=\"struct md5::Md5Core\">Md5Core</a>"]]],["sha1",[["impl <a class=\"trait\" href=\"digest/core_api/trait.FixedOutputCore.html\" title=\"trait digest::core_api::FixedOutputCore\">FixedOutputCore</a> for <a class=\"struct\" href=\"sha1/struct.Sha1Core.html\" title=\"struct sha1::Sha1Core\">Sha1Core</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[13,265]}
+//{"start":57,"fragment_lengths":[13,259,265]}

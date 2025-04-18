87c22b8ffc5c6e6c445960a444dc91a5d799b45d
malept <malept@users.noreply.github.com>
2025-04-18 07:02:13 +0000

    Publish

--- main/trait.impl/core/marker/trait.Sized.js
+++ main/trait.impl/core/marker/trait.Sized.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["bytes",[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"bytes/buf/struct.UninitSlice.html\" title=\"struct bytes::buf::UninitSlice\">UninitSlice</a>",1,["bytes::buf::uninit_slice::UninitSlice"]]]]]);
+    var implementors = Object.fromEntries([["bytes",[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"bytes/buf/struct.UninitSlice.html\" title=\"struct bytes::buf::UninitSlice\">UninitSlice</a>",1,["bytes::buf::uninit_slice::UninitSlice"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/trait.impl/core/ops/bit/trait.Not.js
+++ main/trait.impl/core/ops/bit/trait.Not.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["typenum",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"typenum/bit/struct.B0.html\" title=\"struct typenum::bit::B0\">B0</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"typenum/bit/struct.B1.html\" title=\"struct typenum::bit::B1\">B1</a>"]]]]);
+    var implementors = Object.fromEntries([["typenum",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"typenum/bit/struct.B0.html\" title=\"struct typenum::bit::B0\">B0</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"typenum/bit/struct.B1.html\" title=\"struct typenum::bit::B1\">B1</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
